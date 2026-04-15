5e70c7e4c9a19bf0cb95cdc1e09dd0dcac44aa70
DiamondPie <thelostdiamondpie@outlook.com>
2026-04-15 14:32:19 +1200

    Refactor font imports and update AsmEditor styles to use CSS variables

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Google_Sans_Code } from "next/font/google";
 import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
+const googleSansCode = Google_Sans_Code({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -21,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${googleSansCode.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         {children}
